1a41af27e7769b7374fdd866547001c2752cf4f2
Michael Gragg <mgragg@mjp.com>
2022-10-12 20:21:10 -0400

    Updates

--- precache-manifest.1ce3c2eedf5665b0347365df23656297.js
+++ precache-manifest.1ce3c2eedf5665b0347365df23656297.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5cb515c591f1b89af704564141b5210c",
+    "revision": "a2cbee0cf8b33534f05cf4cdc136252b",
     "url": "/sleeper-ff-rankings/index.html"
   },
   {
-    "revision": "378b932362f2caab4104",
-    "url": "/sleeper-ff-rankings/static/css/main.65193de4.chunk.css"
+    "revision": "29845a009169992a544e",
+    "url": "/sleeper-ff-rankings/static/css/main.86b84169.chunk.css"
   },
   {
     "revision": "076cd26319fdf265bfd9",
     "url": "/sleeper-ff-rankings/static/js/2.c44a549e.chunk.js"
   },
   {
-    "revision": "378b932362f2caab4104",
-    "url": "/sleeper-ff-rankings/static/js/main.4a7d0dba.chunk.js"
+    "revision": "29845a009169992a544e",
+    "url": "/sleeper-ff-rankings/static/js/main.e7e6477f.chunk.js"
   },
   {
     "revision": "3a91b43ec379da4791fd",
