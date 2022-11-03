09af1fba6428e9a150c526e19c6650601f080cb9
Michael Gragg <mgragg@MJP1123.local>
2022-11-03 17:00:10 -0400

    Updates

---
+++ precache-manifest.eb6cf4439e4657bcbf7b4e7097518d65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c54d2bd887d514f4f7cd56b9da4cb0e",
+    "url": "/sleeper-ff-rankings/index.html"
+  },
+  {
+    "revision": "605ca2622931baadee37",
+    "url": "/sleeper-ff-rankings/static/css/main.86b84169.chunk.css"
+  },
+  {
+    "revision": "076cd26319fdf265bfd9",
+    "url": "/sleeper-ff-rankings/static/js/2.c44a549e.chunk.js"
+  },
+  {
+    "revision": "605ca2622931baadee37",
+    "url": "/sleeper-ff-rankings/static/js/main.a157ac9f.chunk.js"
+  },
+  {
+    "revision": "3a91b43ec379da4791fd",
+    "url": "/sleeper-ff-rankings/static/js/runtime~main.0da81726.js"
+  },
+  {
+    "revision": "7214a3e6d8a9b62611ab9acbbab7b15d",
+    "url": "/sleeper-ff-rankings/static/media/logo_with_text.7214a3e6.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sleeper-ff-rankings/precache-manifest.1952435ea2ceb3cd7a4a078c09fa7efa.js"
+  "/sleeper-ff-rankings/precache-manifest.eb6cf4439e4657bcbf7b4e7097518d65.js"
 );
 
 self.addEventListener('message', (event) => {
