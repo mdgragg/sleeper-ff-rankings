f0f5532e6d876ec64e62be1ab64d5faa4daa58ce
Michael Gragg <mgragg@mjp.com>
2023-08-22 17:02:56 -0400

    winning week text style

---
+++ build/precache-manifest.dffa264c1ac891139c8096fb60263910.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60ba7b592e5522be398eba530ad928a2",
+    "url": "/sleeper-ff-rankings/index.html"
+  },
+  {
+    "revision": "a5a45101635f35ff39f2",
+    "url": "/sleeper-ff-rankings/static/css/main.8cb3b4cb.chunk.css"
+  },
+  {
+    "revision": "95cf529b7a744d2f3654",
+    "url": "/sleeper-ff-rankings/static/js/2.05cccbb1.chunk.js"
+  },
+  {
+    "revision": "a5a45101635f35ff39f2",
+    "url": "/sleeper-ff-rankings/static/js/main.0b75ee94.chunk.js"
+  },
+  {
+    "revision": "2b1b7a8ffbe5e2d27d65",
+    "url": "/sleeper-ff-rankings/static/js/runtime~main.fbbc9e38.js"
+  },
+  {
+    "revision": "7214a3e6d8a9b62611ab9acbbab7b15d",
+    "url": "/sleeper-ff-rankings/static/media/logo_with_text.7214a3e6.png"
+  }
+]);
